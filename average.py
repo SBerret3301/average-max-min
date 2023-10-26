365e6c7953aa091ec1ed547920a56119167347e7
salah_eddine_berret <salaheddineberret@gmail.com>
2023-10-26 22:19:28 +0100

    update

--- average.py
+++ average.py
@@ -1,21 +1,22 @@
 t = []
-
-for i in range(5):
+x = int(input("enter the number of student :"))
+y = int(input("enter the number of modules :"))
+for i in range(x):
     t1 = []
-    for j in range(3):
+    for j in range(y):
         print("enter the mark of the student number",i+1,"of the module number",j+1)
         a = float(input())
         t1.append(a)
     t.append(t1)
-for i in range(5) :
+for i in range(x):
     m = 0
-    for j in range(3):
-        m += (t[i][j])/3
+    for j in range(y):
+        m += (t[i][j])/y
     print("the average of the student number",i+1,"is :",m)
-for j in range(3):
+for j in range(y):
     min = t[0][j]
     max = t[0][j]
-    for i in range(5):
+    for i in range(x):
         if min > t[i][j]:
             min = t[i][j]
         if max < t[i][j]:
